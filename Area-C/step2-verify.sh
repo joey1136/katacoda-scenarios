e345ad7c917b9ee4c8aacd3fc261d13a189dbf8a
joey kwok <kwokjoey2000@gmail.com>
2021-11-13 18:37:37 +0800

    update step 2 verify

--- Area-C/step2-verify.sh
+++ Area-C/step2-verify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker exec -it mysql cat etc/my.cnf | grep -c "[mysqld]
-general_log = 1
-log_output = table"
+docker exec -it mysql cat etc/my.cnf | grep -c "general_log = 1"
 
 if [[ $? -eq 0 ]]
   then 
